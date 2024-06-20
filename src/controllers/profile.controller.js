a9275909ced5f8a282c5b7e4b356c65598ed0947
Swapnil Opash <sra.opash@gmail.com>
2024-06-20 18:23:14 +0530

    fixed

--- src/controllers/profile.controller.js
+++ src/controllers/profile.controller.js
@@ -76,7 +76,7 @@ exports.updateProfile = async function (req, res) {
         .json({ error: true, message: "Username is already exist" });
     }
 
-    if (req.body.Id === req.user.id) {
+    if (req.body.profileId === req.user.id) {
       if (req.body.UserID) {
         const updateUserData = {
           Username: reqBody?.Username,
